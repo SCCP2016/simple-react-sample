95a1f997896aed1fb62411b2efbbbe4389df1b49
abab <ababup1192@gmail.com>
2017-01-12 09:31:57 +0900

    Add generate random button.

--- main.jsx
+++ main.jsx
@@ -1,29 +1,39 @@
 class App extends React.Component {
     constructor(props) {
         super(props);
-        // this.props == { name: "abab", list: List.of("aaa", "bbb", "ccc") };
+
+        this.state = { list: this.props.list };
+    }
+
+    handleClick() {
+        const {list} = this.state;
+        const newList = list.push(Math.random().toString(36).slice(-8));
+        
+        this.setState({list: newList});
     }
 
     render() {
-        // Same mean.
-        // const name = this.props.name;
-        // const list = this.props.list;
-        const {name, list} = this.props;
-        // name = "hoge"; <- error.
+        const {name} = this.props;
+        const {list} = this.state;
 
         const indexedList = list.map((x, idx) =>
             <li>{idx}: {x}</li>
         );
 
         return <div>
-                 <h1>Hello {name} !</h1>
-                 <ul>
-                   { indexedList }
-                 </ul>
-               </div>;
+            <h1>Hello {name} ! Welcome to React World!</h1>
+            <input
+                value="push!"
+                type="button"
+                onClick={() => this.handleClick()}
+                />
+            <ul>
+                {indexedList}
+            </ul>
+        </div>;
     }
 }
 
 const List = Immutable.List;
-const list = List.of("aaa", "bbb", "ccc");
+const list = List.of();
 ReactDOM.render(<App name="abab" list={list} />, document.getElementById("app"));
